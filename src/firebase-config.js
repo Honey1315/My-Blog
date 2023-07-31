1a94b8b679fa664f8cb0a7fc42a62730b9cbe741
Honey1315 <99478938+Honey1315@users.noreply.github.com>
2023-07-31 23:11:16 +0530

    Update firebase-config.js

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -7,12 +7,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyB6m2S9AYmZ2geFRigsh-a4TizxTkUd_cQ",
-  authDomain: "myblog-6f20d.firebaseapp.com",
-  projectId: "myblog-6f20d",
-  storageBucket: "myblog-6f20d.appspot.com",
-  messagingSenderId: "160102063803",
-  appId: "1:160102063803:web:6a8704d4fc1ebce735139e"
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_API_ID,
 };
 
 // Initialize Firebase
